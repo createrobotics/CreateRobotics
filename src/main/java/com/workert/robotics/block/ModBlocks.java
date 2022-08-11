2a843086caf56db78e881692dfce7995c486670c
OutCraft-Mods <yt.outcraft@gmail.com>
2022-08-11 12:09:22 +0200

    Clean up and add function to register Items
    Signed-off-by: OutCraft-Mods <yt.outcraft@gmail.com>

--- src/main/java/com/workert/robotics/block/ModBlocks.java
+++ src/main/java/com/workert/robotics/block/ModBlocks.java
@@ -19,44 +19,45 @@ import net.minecraftforge.registries.RegistryObject;
 import java.util.function.Supplier;
 
 public class ModBlocks {
-    public  static  final DeferredRegister<Block> BLOCKS =
-            DeferredRegister.create(ForgeRegistries.BLOCKS, Robotics.MOD_ID);
+	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
+			Robotics.MOD_ID);
 
-    public static final RegistryObject<Block> TIN_ORE = registerBlock("tin_ore",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(9f).requiresCorrectToolForDrops()), ModCreativeModeTab.ROBOTICS_TAB);
+	public static final RegistryObject<Block> TIN_ORE = registerBlock("tin_ore",
+			() -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(9f).requiresCorrectToolForDrops()),
+			ModCreativeModeTab.ROBOTICS_TAB);
 
-    public static final RegistryObject<Block> TIN_BLOCK = registerBlock("tin_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(9f).requiresCorrectToolForDrops()), ModCreativeModeTab.ROBOTICS_TAB);
+	public static final RegistryObject<Block> TIN_BLOCK = registerBlock("tin_block",
+			() -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(9f).requiresCorrectToolForDrops()),
+			ModCreativeModeTab.ROBOTICS_TAB);
 
-    public static final RegistryObject<Block> BRONZE_BLOCK = registerBlock("bronze_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(9f).requiresCorrectToolForDrops()), ModCreativeModeTab.ROBOTICS_TAB);
+	public static final RegistryObject<Block> BRONZE_BLOCK = registerBlock("bronze_block",
+			() -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(9f).requiresCorrectToolForDrops()),
+			ModCreativeModeTab.ROBOTICS_TAB);
 
-    public static final RegistryObject<Block> DEEPSLATE_TIN_ORE = registerBlock("deepslate_tin_ore",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(9f).requiresCorrectToolForDrops()), ModCreativeModeTab.ROBOTICS_TAB);
+	public static final RegistryObject<Block> DEEPSLATE_TIN_ORE = registerBlock("deepslate_tin_ore",
+			() -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(9f).requiresCorrectToolForDrops()),
+			ModCreativeModeTab.ROBOTICS_TAB);
 
-    public static final RegistryObject<Block> SMASHER_BLOCK = registerBlock("smasher_block",
-            () -> new SmasherBlock(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(9f).requiresCorrectToolForDrops()), ModCreativeModeTab.ROBOTICS_TAB);
+	public static final RegistryObject<Block> SMASHER_BLOCK = registerBlock("smasher_block",
+			() -> new SmasherBlock(
+					BlockBehaviour.Properties.of(Material.METAL).strength(9f).requiresCorrectToolForDrops()),
+			ModCreativeModeTab.ROBOTICS_TAB);
 
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab ){
-        RegistryObject<T> toReturn = BLOCKS.register(name, block);
-        registerBlockItem(name, toReturn, tab);
-        return toReturn;
-    }
+	private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block,
+			CreativeModeTab tab) {
+		RegistryObject<T> toReturn = BLOCKS.register(name, block);
+		registerBlockItem(name, toReturn, tab);
+		return toReturn;
+	}
 
-    private static <T extends Block>RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
-                                                                           CreativeModeTab tab){
+	private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+			CreativeModeTab tab) {
 
-        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
-                new Item.Properties().tab(tab)));
-    }
+		return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+	}
 
-    public  static  void register(IEventBus eventBus) {
-        BLOCKS.register(eventBus);
+	public static void register(IEventBus eventBus) {
+		BLOCKS.register(eventBus);
 
-    }
+	}
 }
