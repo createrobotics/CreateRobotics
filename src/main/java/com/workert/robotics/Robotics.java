ce6dea5ae610dc91d08316254fdc2cad2933e782
OutCraft-Mods <yt.outcraft@gmail.com>
2022-08-11 11:19:34 +0200

    Remove unnecessary code
    Signed-off-by: OutCraft-Mods <yt.outcraft@gmail.com>

--- src/main/java/com/workert/robotics/Robotics.java
+++ src/main/java/com/workert/robotics/Robotics.java
@@ -1,51 +1,41 @@
 package com.workert.robotics;
 
+import org.slf4j.Logger;
+
 import com.mojang.logging.LogUtils;
 import com.workert.robotics.block.ModBlocks;
 import com.workert.robotics.block.entity.ModBlockEntities;
 import com.workert.robotics.item.ModItems;
+
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 
 @Mod(Robotics.MOD_ID)
-public class Robotics
-{
+public class Robotics {
 
-    public static final String MOD_ID = "robotics";
-    private static final Logger LOGGER = LogUtils.getLogger();
+	public static final String MOD_ID = "robotics";
+	private static final Logger LOGGER = LogUtils.getLogger();
 
+	public Robotics() {
+		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-    public Robotics() {
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		ModBlocks.register(eventBus);
+		ModItems.register(eventBus);
 
-        ModBlocks.register(eventBus);
-        ModItems.register(eventBus);
+		ModBlockEntities.register(eventBus);
 
-        ModBlockEntities.register(eventBus);
+		eventBus.addListener(this::clientSetup);
 
+		MinecraftForge.EVENT_BUS.register(this);
 
-        eventBus.addListener(this::setup);
-        eventBus.addListener(this::clientSetup);
+	}
 
-        MinecraftForge.EVENT_BUS.register(this);
-
-    }
-
-    private void clientSetup(final FMLClientSetupEvent event) {
-            ItemBlockRenderTypes.setRenderLayer(ModBlocks.SMASHER_BLOCK.get(), RenderType.translucent());
-    }
-
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
+	private void clientSetup(final FMLClientSetupEvent event) {
+		ItemBlockRenderTypes.setRenderLayer(ModBlocks.SMASHER_BLOCK.get(), RenderType.translucent());
+	}
 }
