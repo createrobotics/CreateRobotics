c913e661573c4cfc4387d874cb11c3157dd022a7
RealDarkGamer <94769968+RealDarkStudios@users.noreply.github.com>
2023-01-05 05:38:14 -0600

    More Extend-O-Boots work

--- src/main/java/com/workert/robotics/items/ExtendOBootsItem.java
+++ src/main/java/com/workert/robotics/items/ExtendOBootsItem.java
@@ -14,14 +14,15 @@ import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL;
 
 @Mod.EventBusSubscriber(modid = Robotics.MOD_ID)
 public class ExtendOBootsItem extends ArmorItem {
-    private static boolean activated = false;
-    private static int currentHeight = 0;
+    private boolean activated, lControlHeld = false;
+    private int currentHeight = 0;
     private static final int MAX_HEIGHT = 10;
-    private static double pX, pY, pZ;
-    private static Player plr;
+    private double pX, pY, pZ;
+    private Player plr;
 
     public ExtendOBootsItem(ArmorMaterial pMaterial, EquipmentSlot pSlot, Properties pProperties) {
         super(pMaterial, pSlot, pProperties);
@@ -32,38 +33,49 @@ public class ExtendOBootsItem extends ArmorItem {
         super.onArmorTick(stack, level, player);
         if (!level.isClientSide()) {
             plr = player;
-            if (activated) {
-                teleport(player, pX, pY + currentHeight, pZ, player.getXRot(), player.getYRot());
+            if (activated && Math.abs(player.getX() - pX) < 0.0025d && Math.abs(player.getZ() - pZ) < 0.0025d) {
+                teleport(player, player.getX(), pY + currentHeight, player.getZ());
+            } else if (activated) {
+                currentHeight = 0;
+                activated = false;
+                teleport(player, player.getX(), pY, player.getZ());
+                System.out.println("DEACTIVATING");
             }
         }
     }
 
     @SubscribeEvent
-    public static void detectScroll(InputEvent.MouseScrollingEvent mouseEvent) {
-        if (mouseEvent.getScrollDelta() > 0 && currentHeight < 10 && activated) {
+    public void detectScroll(InputEvent.MouseScrollingEvent mouseEvent) {
+        if (mouseEvent.getScrollDelta() > 0 && currentHeight < 10 && activated && lControlHeld) {
             System.out.println("New height, " + currentHeight);
             currentHeight += 1;
-        } else if (mouseEvent.getScrollDelta() < 0 && currentHeight > 0 && activated) {
+        } else if (mouseEvent.getScrollDelta() < 0 && currentHeight > 0 && activated && lControlHeld) {
             currentHeight -= 1;
             System.out.println("New height, " + currentHeight);
         }
     }
 
     @SubscribeEvent
-    public static void detectInput(InputEvent.Key event) {
-        if (event.getKey() == GLFW.GLFW_KEY_LEFT_CONTROL && event.getAction() == InputConstants.REPEAT) {
+    public void detectInput(InputEvent.Key event) {
+        if (event.getKey() == GLFW.GLFW_KEY_LEFT_CONTROL && event.getAction() == InputConstants.PRESS && plr.isOnGround()) {
             System.out.println("ACTIVATED, currHeight: " + currentHeight);
             pX = plr.getX();
             pY = plr.getY();
             pZ = plr.getZ();
             activated = true;
-        } else if (event.getKey() == GLFW.GLFW_KEY_LEFT_CONTROL && event.getAction() == InputConstants.REPEAT && currentHeight == 0) {
+        } else if (event.getKey() == GLFW.GLFW_KEY_LEFT_CONTROL && event.getAction() == InputConstants.RELEASE && currentHeight == 0) {
             System.out.println("DEACTIVATED, currHeight: " + currentHeight);
             activated = false;
         }
+
+        if (event.getKey() == GLFW.GLFW_KEY_LEFT_CONTROL && event.getAction() == InputConstants.PRESS) {
+            lControlHeld = true;
+        } else if (event.getKey() == GLFW.GLFW_KEY_LEFT_CONTROL && event.getAction() == InputConstants.RELEASE) {
+            lControlHeld = false;
+        }
     }
 
-    public static void teleport(Player player, double pX, double pY, double pZ, float xRot, float yRot) {
-        player.moveTo(pX, pY, pZ, yRot, xRot);
+    public static void teleport(Player player, double pX, double pY, double pZ) {
+        player.teleportTo(pX, pY, pZ);
     }
 }
