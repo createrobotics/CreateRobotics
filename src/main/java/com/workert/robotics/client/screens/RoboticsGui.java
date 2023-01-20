02f0db929f3d9162131d0a1c236a38daac31dae9
WORKERT <snayderxp18@gmail.com>
2023-01-20 11:22:28 -0500

    RoboticsGuis

---
+++ src/main/java/com/workert/robotics/client/screens/RoboticsGui.java
@@ -0,0 +1,13 @@
+package com.workert.robotics.client.screens;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.simibubi.create.foundation.gui.element.ScreenElement;
+
+public enum RoboticsGui implements ScreenElement {
+    ;
+
+    @Override
+    public void render(PoseStack ms, int x, int y) {
+
+    }
+}
