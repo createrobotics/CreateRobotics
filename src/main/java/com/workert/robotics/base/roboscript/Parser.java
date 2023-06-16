1e80b5797a3425c08964a37812a8caec8bcaf178
light672 <99850744+light672@users.noreply.github.com>
2023-06-16 17:04:08 -0500

    Fixed initializers having random parsing errors.

--- src/main/java/com/workert/robotics/base/roboscript/Parser.java
+++ src/main/java/com/workert/robotics/base/roboscript/Parser.java
@@ -65,7 +65,7 @@ public final class Parser {
 				fields.add((Statement.Var) this.varDeclaration(false));
 				continue;
 			} else if (this.advanceIfNextTokenMatches(
-					Token.TokenType.IDENTIFIER) && this.getPreviousToken().lexeme == name.lexeme) {
+					Token.TokenType.IDENTIFIER) && this.getPreviousToken().lexeme.equals(name.lexeme)) {
 				initializer = this.function(this.getPreviousToken(), "initializer");
 				continue;
 			} else {
