6cbeb77005a593050f40977d442e8412c735a667
light672 <99850744+light672@users.noreply.github.com>
2023-07-08 14:34:13 -0500

    Fixed OP_NOT having an unnecessary try catch block

--- src/main/java/com/workert/robotics/base/roboscriptbytecode/VirtualMachine.java
+++ src/main/java/com/workert/robotics/base/roboscriptbytecode/VirtualMachine.java
@@ -43,13 +43,7 @@ final class VirtualMachine {
 				case OP_SUBTRACT -> this.binaryOperation('-');
 				case OP_MULTIPLY -> this.binaryOperation('*');
 				case OP_DIVIDE -> this.binaryOperation('/');
-				case OP_NOT -> {
-					try {
-						this.stack.set(this.stack.size() - 1, !isTruthy(this.stack.peek()));
-					} catch (ClassCastException e) {
-						throw new RuntimeError("Can only toggle booleans.");
-					}
-				}
+				case OP_NOT -> this.stack.set(this.stack.size() - 1, !isTruthy(this.stack.peek()));
 				case OP_NEGATE -> {
 					try {
 						this.stack.set(this.stack.size() - 1, -(double) this.stack.peek());
