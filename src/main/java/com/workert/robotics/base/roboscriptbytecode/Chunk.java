687284eb5077307355c2072349f0355916b327cb
light672 <99850744+light672@users.noreply.github.com>
2023-07-13 17:29:32 -0500

    Changed the ByteArrayOutputStream to a custom one in Chunk.

--- src/main/java/com/workert/robotics/base/roboscriptbytecode/Chunk.java
+++ src/main/java/com/workert/robotics/base/roboscriptbytecode/Chunk.java
@@ -1,12 +1,16 @@
 package com.workert.robotics.base.roboscriptbytecode;
-import java.io.ByteArrayOutputStream;
+import jdk.internal.util.ArraysSupport;
+
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 final class Chunk {
-	private ByteArrayOutputStream code = new ByteArrayOutputStream();
+	private RoboByteArrayOutputStream code = new RoboByteArrayOutputStream();
 	private List<Object> constants = new ArrayList<>();
-	private List<Object> variables = new ArrayList<>();
 	private List<Integer> lines = new ArrayList<>();
 
 
@@ -14,6 +18,10 @@ final class Chunk {
 	void writeCode(byte code, int line) {
 		this.code.write(code);
 		this.lines.add(line);
+	}
+
+	void setCode(int index, byte code) {
+		this.code.buf[index] = code;
 	}
 
 	byte readCode(int i) {
@@ -31,16 +39,77 @@ final class Chunk {
 		return this.constants.size() - 1;
 	}
 
+
 	Object readConstant(int i) {
-		return this.constants.get(i);
+		return this.constants.get(Math.abs(i));
 	}
 
-
-	Object readVariable(byte variable) {
-		return this.variables.get(variable);
-	}
 
 	int readLine(int i) {
 		return this.lines.get(i);
 	}
+
+
+	public static class RoboByteArrayOutputStream extends OutputStream {
+
+
+		private byte buf[];
+
+
+		private int count;
+
+		public RoboByteArrayOutputStream() {
+			this.buf = new byte[32];
+		}
+
+
+		private void ensureCapacity(int minCapacity) {
+			// overflow-conscious code
+			int oldCapacity = this.buf.length;
+			int minGrowth = minCapacity - oldCapacity;
+			if (minGrowth > 0) {
+				this.buf = Arrays.copyOf(this.buf, ArraysSupport.newLength(oldCapacity,
+						minGrowth, oldCapacity /* preferred growth */));
+			}
+		}
+
+
+		@Override
+		public synchronized void write(int b) {
+			this.ensureCapacity(this.count + 1);
+			this.buf[this.count] = (byte) b;
+			this.count += 1;
+		}
+
+		@Override
+		public synchronized void write(byte b[], int off, int len) {
+			Objects.checkFromIndexSize(off, len, b.length);
+			this.ensureCapacity(this.count + len);
+			System.arraycopy(b, off, this.buf, this.count, len);
+			this.count += len;
+		}
+
+		public void writeToByte(int index, byte b) {
+			this.buf[index] = b;
+		}
+
+		public synchronized byte[] toByteArray() {
+			return Arrays.copyOf(this.buf, this.count);
+		}
+
+		public synchronized int size() {
+			return this.count;
+		}
+
+		@Override
+		public String toString() {
+			return this.buf.toString();
+		}
+
+		@Override
+		public void close() throws IOException {
+		}
+
+	}
+
 }
