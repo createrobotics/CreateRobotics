8a264b9cd1de49fb2832f41288b1837e6941c55f
light672 <99850744+light672@users.noreply.github.com>
2023-07-31 11:14:23 -0500

    Fixed classes sharing the same hashmap

--- src/main/java/com/workert/robotics/base/roboscriptbytecode/RoboScriptObject.java
+++ src/main/java/com/workert/robotics/base/roboscriptbytecode/RoboScriptObject.java
@@ -1,10 +1,11 @@
 package com.workert.robotics.base.roboscriptbytecode;
+import java.util.HashMap;
 import java.util.Map;
 
 public class RoboScriptObject {
 	Map<String, Object> fields;
 
 	RoboScriptObject(Map<String, Object> fields) {
-		this.fields = fields;
+		this.fields = new HashMap<>(fields);
 	}
 }
