ff6f246b2a5083419e017451384e8679e6ff8f68
OutCraft-Mods <yt.outcraft@gmail.com>
2023-08-02 17:05:36 +0200

    Fixed issues with Mixin Remappings not working outside of Development Environment and Model Layer Locations not registering on Client

--- src/main/java/com/workert/robotics/mixin/ToolboxTileEntityMixin.java
+++ src/main/java/com/workert/robotics/mixin/ToolboxTileEntityMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = ToolboxTileEntity.class, remap = false)
 public abstract class ToolboxTileEntityMixin {
-	@Redirect(method = {"updateOpenCount", "tickAudio"}, at = @At(value = "FIELD", opcode = Opcodes.GETFIELD, target = "Lcom/simibubi/create/content/curiosities/toolbox/ToolboxTileEntity;worldPosition:Lnet/minecraft/core/BlockPos;"))
+	@Redirect(method = {"updateOpenCount", "tickAudio"}, at = @At(value = "FIELD", opcode = Opcodes.GETFIELD, target = "Lcom/simibubi/create/content/curiosities/toolbox/ToolboxTileEntity;worldPosition:Lnet/minecraft/core/BlockPos;", remap = true))
 	BlockPos worldPosition(ToolboxTileEntity instance) {
 		return instance.getBlockPos();
 	}
