dc238391c19123c5ab5f5cbcc1c54ead27d0e8a6
OutCraft-Mods <yt.outcraft@gmail.com>
2023-08-10 21:20:24 +0200

    Cleaned up RoboScriptRunner class

--- src/main/java/com/workert/robotics/base/roboscript/tool/RoboScriptRunner.java
+++ src/main/java/com/workert/robotics/base/roboscript/tool/RoboScriptRunner.java
@@ -2,24 +2,11 @@ package com.workert.robotics.base.roboscript.tool;
 import com.workert.robotics.base.roboscript.RoboScript;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class RoboScriptRunner {
-
-	// credit to Crafting Interpreters and the cLox language for most of the understanding of bytecode interpreters.
-	// check it out at https://craftinginterpreters.com.
-
-	// credit to jakslxpwrm on discord for helping with optimizations and a lot of C to Java conversions
-
 	private static final String sourcePath = "src/main/java/com/workert/robotics/base/roboscript/tool/script.robo";
-
-
-	private static String readFile() throws IOException {
-		byte[] bytes = Files.readAllBytes(Paths.get(sourcePath));
-		return new String(bytes, Charset.defaultCharset());
-	}
 
 	public static void main(String[] args) throws IOException {
 		new RoboScript() {
@@ -32,6 +19,6 @@ public class RoboScriptRunner {
 			protected void handleErrorMessage(String error) {
 				System.err.println(error);
 			}
-		}.runString(readFile());
+		}.runString(Files.readString(Paths.get(sourcePath)));
 	}
 }
