6c0d4a26cf8aa2899b39fc92968393e83ccdb1d9
OutCraft-Mods <yt.outcraft@gmail.com>
2023-08-11 23:10:32 +0200

    Fixed compiling issues in the ComputerBlockEntity class

--- src/main/java/com/workert/robotics/content/computers/computer/ComputerBlockEntity.java
+++ src/main/java/com/workert/robotics/content/computers/computer/ComputerBlockEntity.java
@@ -6,10 +6,11 @@ import com.simibubi.create.content.logistics.RedstoneLinkNetworkHandler;
 import com.simibubi.create.foundation.tileEntity.TileEntityBehaviour;
 import com.simibubi.create.foundation.utility.Couple;
 import com.workert.robotics.base.roboscript.RoboScript;
-import com.workert.robotics.base.roboscript.RoboScriptArgumentPredicates;
 import com.workert.robotics.base.roboscript.RuntimeError;
 import com.workert.robotics.base.roboscript.VirtualMachine;
 import com.workert.robotics.base.roboscript.ingame.LineLimitedString;
+import com.workert.robotics.base.roboscript.util.RoboScriptArgumentPredicates;
+import com.workert.robotics.base.roboscript.util.RoboScriptObjectConversions;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
@@ -60,10 +61,10 @@ public class ComputerBlockEntity extends KineticTileEntity {
 				super.defineNativeFunctions();
 				this.defineNativeFunction("display", 1, (args) -> {
 					if (args[0] instanceof List l) {
-						ComputerBlockEntity.this.outputDisplay = RoboScriptArgumentPredicates.stringifyAllElements(l);
+						ComputerBlockEntity.this.outputDisplay = RoboScriptObjectConversions.stringifyAllElements(l);
 						return null;
 					}
-					ComputerBlockEntity.this.outputDisplay = List.of(RoboScriptArgumentPredicates.stringify(args[0]));
+					ComputerBlockEntity.this.outputDisplay = List.of(RoboScriptObjectConversions.stringify(args[0]));
 					return null;
 				});
 				this.defineNativeFunction("emitLinkSignal", 3, (args) -> {
