ceb11b6ecf0d21a9cc9dc53a30faa9b1975281d2
light672 <99850744+light672@users.noreply.github.com>
2023-08-12 16:15:42 -0500

    Removed anything to do with links emitting from a computer.

--- src/main/java/com/workert/robotics/content/computers/computer/ComputerBlockEntity.java
+++ src/main/java/com/workert/robotics/content/computers/computer/ComputerBlockEntity.java
@@ -1,20 +1,13 @@
 package com.workert.robotics.content.computers.computer;
 
-import com.simibubi.create.Create;
 import com.simibubi.create.content.contraptions.base.KineticTileEntity;
-import com.simibubi.create.content.logistics.RedstoneLinkNetworkHandler;
-import com.simibubi.create.foundation.tileEntity.TileEntityBehaviour;
-import com.simibubi.create.foundation.utility.Couple;
 import com.workert.robotics.base.roboscript.RoboScript;
 import com.workert.robotics.base.roboscript.ingame.LineLimitedString;
-import com.workert.robotics.base.roboscript.util.RoboScriptArgumentPredicates;
 import com.workert.robotics.base.roboscript.util.RoboScriptObjectConversions;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.StringTag;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -31,10 +24,6 @@ public class ComputerBlockEntity extends KineticTileEntity {
 	private boolean running = false;
 
 	private List<String> outputDisplay = new ArrayList<>();
-
-	private ComputerRedstoneLinkBehavior redstoneLinkBehavior = new ComputerRedstoneLinkBehavior(0, Couple.create(
-			RedstoneLinkNetworkHandler.Frequency.of(ItemStack.EMPTY),
-			RedstoneLinkNetworkHandler.Frequency.of(ItemStack.EMPTY)), this.getBlockPos());
 
 
 	public ComputerBlockEntity(BlockEntityType<?> type, BlockPos blockPos, BlockState blockState) {
@@ -64,41 +53,10 @@ public class ComputerBlockEntity extends KineticTileEntity {
 					ComputerBlockEntity.this.outputDisplay = List.of(RoboScriptObjectConversions.stringify(args[0]));
 					return null;
 				});
-				this.defineNativeFunction("setLinkPower", 1, (args) -> {
-					int signalStrength = RoboScriptArgumentPredicates.asPositiveFullNumber(args[0], true);
-
-					ComputerBlockEntity.this.redstoneLinkBehavior.signalStrength = Math.min(
-							signalStrength, 15);
-
-					Create.REDSTONE_LINK_NETWORK_HANDLER.updateNetworkOf(ComputerBlockEntity.this.level,
-							ComputerBlockEntity.this.redstoneLinkBehavior);
-					return null;
-				});
-				this.defineNativeFunction("setLinkFrequency", 2, (args) -> {
-					Create.REDSTONE_LINK_NETWORK_HANDLER.removeFromNetwork(ComputerBlockEntity.this.level,
-							ComputerBlockEntity.this.redstoneLinkBehavior);
-
-					Item item1 = RoboScriptArgumentPredicates.asItem(args[0]);
-					Item item2 = RoboScriptArgumentPredicates.asItem(args[1]);
-
-					// TODO Use RedstoneLinkNetworkHandler.Frequency.EMPTY when null asItem
-					ComputerBlockEntity.this.redstoneLinkBehavior.frequency = Couple.create(
-							RedstoneLinkNetworkHandler.Frequency.of(item1.getDefaultInstance()),
-							RedstoneLinkNetworkHandler.Frequency.of(item2.getDefaultInstance()));
-
-					Create.REDSTONE_LINK_NETWORK_HANDLER.addToNetwork(ComputerBlockEntity.this.level,
-							ComputerBlockEntity.this.redstoneLinkBehavior);
-					return null;
-				});
 			}
 		};
 	}
 
-	@Override
-	public void onLoad() {
-		super.onLoad();
-		Create.REDSTONE_LINK_NETWORK_HANDLER.addToNetwork(this.getLevel(), this.redstoneLinkBehavior);
-	}
 
 	public void runScript() {
 		this.roboScript.runString(this.script);
@@ -130,11 +88,6 @@ public class ComputerBlockEntity extends KineticTileEntity {
 		compound.putString("Script", this.script);
 		compound.putString("Terminal", this.terminal.getString());
 		compound.putBoolean("Running", this.running);
-	}
-
-	@Override
-	public void addBehavioursDeferred(List<TileEntityBehaviour> behaviours) {
-		super.addBehavioursDeferred(behaviours);
 	}
 
 	@Override
