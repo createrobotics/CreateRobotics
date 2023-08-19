eae094980a3eae6b353b917992cdf4731a7569a0
light672 <99850744+light672@users.noreply.github.com>
2023-08-19 11:01:37 -0500

    Revert "Removed unneeded things in native methods."
    This reverts commit a30426fee9494505678a76815bda57243936982f.

--- src/main/java/com/workert/robotics/base/roboscript/RoboScriptNativeMethod.java
+++ src/main/java/com/workert/robotics/base/roboscript/RoboScriptNativeMethod.java
@@ -1,8 +1,10 @@
 package com.workert.robotics.base.roboscript;
-public abstract class RoboScriptNativeMethod {
+public abstract class RoboScriptNativeMethod<T> {
+	T object;
 	byte argumentCount;
 
-	RoboScriptNativeMethod(byte argumentCount) {
+	RoboScriptNativeMethod(T object, byte argumentCount) {
+		this.object = object;
 		this.argumentCount = argumentCount;
 	}
 
