a2715b9fe18eb45b9430b6ff0aa7f85cecd31772
light672 <99850744+light672@users.noreply.github.com>
2023-08-19 18:19:14 -0500

    Removed stack being logged on print function.

--- src/main/java/com/workert/robotics/base/roboscript/RoboScript.java
+++ src/main/java/com/workert/robotics/base/roboscript/RoboScript.java
@@ -4,7 +4,6 @@ import com.workert.robotics.base.roboscript.util.RoboScriptArgumentPredicates;
 import com.workert.robotics.base.roboscript.util.RoboScriptObjectConversions;
 
 import javax.annotation.Nonnull;
-import java.util.Arrays;
 
 public abstract class RoboScript {
 	Compiler compiler = new Compiler(this);
@@ -35,7 +34,6 @@ public abstract class RoboScript {
 	protected void defineNativeFunctions() {
 		this.defineNativeFunction("print", 1, args -> {
 			RoboScript.this.handlePrintMessage(RoboScriptObjectConversions.stringify(args[0]));
-			System.out.println(Arrays.toString(this.virtualMachine.stack));
 			return null;
 		});
 
